03d373e0e11c6eaf67d1ed7c26f5a7d50196d7da
Acer <Acer@DESKTOP-17F2MQ1>
2022-11-15 21:46:00 +0530

    Multiplicatiion push code.

---
+++ src/Com/StackN/Java/Datatypes/Datatype1.java
@@ -0,0 +1,17 @@
+package Com.StackN.Java.Datatypes;
+
+public class Datatype1 
+{
+	public static void main (String []args)
+	{
+		byte a=15;
+		short b=30;
+		int c;
+		
+			 c=a*b;
+		System.out.println(c);
+		
+	}
+	
+
+}
